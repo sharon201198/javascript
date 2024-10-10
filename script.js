78c6d32fe37147059f0bafc5cee7450abeb5ab11
sharon201198 <86478728+sharon201198@users.noreply.github.com>
2024-10-10 12:50:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function submitForm() {
+    var userName = document.forms["loginForm"]["firstname"].value;
+    var passWord = document.forms["loginForm"]["lastname"].value;
+    console.log(userName)
+    
+  }
